0e92ffcea37d5577069046a1b1bc94927145ccbf
Velvid <muntakim.ca@gmail.com>
2022-12-04 15:19:26 -0500

    tidy up styling a bit

--- 2020/day04/solution.py
+++ 2020/day04/solution.py
@@ -16,8 +16,13 @@ def read_file(filename:str) -> str:
     return data.strip() # remove leading/trailing whitespace
 
 
-required_fields = set(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"])
-feild_critera = { # dictionary of lambda functions
+def parse_data(filename:str) -> typing.List[typing.List[str]]:
+    data = read_file(filename)
+    return [entry.split() for entry in data.split("\n\n")]
+
+
+required_fields = set("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+match_criteria = { # dictionary of lambda functions
     "byr": lambda x: (1920 <= int(x) <= 2002),
     "iyr": lambda x: 2010 <= int(x) <= 2020,
     "eyr": lambda x: 2020 <= int(x) <= 2030,
@@ -26,14 +31,8 @@ feild_critera = { # dictionary of lambda functions
     "hcl": lambda x: x.startswith("#") and len(x) == 7 and all(c in "0123456789abcdef" for c in x[1:]),
     "ecl": lambda x: x in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"],
     "pid": lambda x: len(x) == 9 and x.isdigit(),
-    "cid": lambda x: True
+    "cid": lambda x: True # always true, since it's an optional field
 }
-
-
-# TODO appropriately change return type
-def parse_data(filename:str) -> typing.List[typing.List[str]]:
-    data = read_file(filename)
-    return [entry.split() for entry in data.split("\n\n")]
 
 
 def part1() -> int:
@@ -42,7 +41,7 @@ def part1() -> int:
     valid_passports = 0
 
     for passport in passports:
-        current_fields = set([field.split(":")[0] for field in passport])
+        current_fields = set(field.split(":")[0] for field in passport)
         missing_fields = required_fields - current_fields
         if not missing_fields:
             valid_passports += 1
@@ -56,18 +55,15 @@ def part2() -> int:
     valid_passports = 0
 
     for passport in passports:
-        current_fields = set([field.split(":")[0] for field in passport])
+        current_fields = set(field.split(":")[0] for field in passport)
         missing_fields = required_fields - current_fields
-
-        if missing_fields:
-            continue
-
-        for field in passport:
-            key, value = field.split(":")
-            if not feild_critera[key](value):
-                break
-        else: # if no break
-            valid_passports += 1
+        if not missing_fields:
+            for field in passport:
+                key, value = field.split(":")
+                if not match_criteria[key](value): # match_criteria[key] looks up a function
+                    break
+            else: # if no break
+                valid_passports += 1
 
     return valid_passports
 
