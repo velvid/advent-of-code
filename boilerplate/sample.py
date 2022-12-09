b7e324878c536a21691774ee832ead928e957894
Velvid <muntakim.ca@gmail.com>
2022-12-09 16:18:48 -0500

    update boilerplate sample; remove redundant parse_data calls

--- boilerplate/sample.py
+++ boilerplate/sample.py
@@ -1,36 +1,36 @@
 
 
 import os
-from typing import List, Tuple
 
 
-def read_file(filename:str) -> str:
+def read_file(filename: str) -> str:
     dir_path = os.path.dirname(os.path.realpath(__file__))
     file_path = os.path.join(dir_path, filename)
     try:
         with open(file_path) as file:
             data = file.read()
     except FileNotFoundError:
-        raise FileNotFoundError(f"{filename} not in same directory as solution.py")
-    return data.strip() # remove leading/trailing whitespace
+        raise FileNotFoundError(
+            f"{filename} not in same directory as solution.py")
+    return data
 
 
-def parse_data(filename:str) -> List[str]: # TODO appropriately change return type
+# TODO appropriately change return type
+def parse_data(filename: str):
     data = read_file(filename)
     lines = [line.strip() for line in data.splitlines()]
     return lines
 
 
-def part1() -> int:
-    data = parse_data("test.txt")
-    return data # TODO: this isn't an int, it's a list of strings
+def part1(data) -> int:
+    return -1
 
 
-def part2() -> int:
-    data = parse_data("test.txt")
-    return data # TODO: this isn't an int, it's a list of strings
+def part2(data) -> int:
+    return -1
 
 
 if __name__ == '__main__':
-    print("part 1:", part1())
-    print("part 2:", part2())
+    data = parse_data("test.txt")
+    print("part 1:", part1(data))
+    print("part 2:", part2(data))
