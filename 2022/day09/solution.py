19da82c7895f5468265d2148db0f67db5782fb50
Velvid <muntakim.ca@gmail.com>
2022-12-09 16:05:36 -0500

    playing around with argparse

--- 2022/day09/solution.py
+++ 2022/day09/solution.py
@@ -2,6 +2,7 @@
 
 
 import os
+import argparse
 
 
 def read_file(filename: str) -> str:
@@ -12,19 +13,13 @@ def read_file(filename: str) -> str:
             data = file.read()
     except FileNotFoundError:
         raise FileNotFoundError(
-            f"{filename} not in same directory as solution.py")
+            f"{filename} not in same directory as {os.path.basename(__file__)}")
     return data.strip()
 
 
 def parse_data(filename: str) -> list[tuple[str, int]]:
     data = read_file(filename)
-    lines = [line for line in data.splitlines()]
-    moves = []
-    for line in lines:
-        dir = line.split()[0]
-        dist = line.split()[1]
-        moves.append((dir, int(dist)))
-    return moves
+    return [(line.split()[0], int(line.split()[1])) for line in data.splitlines()]
 
 
 class vec2:
@@ -32,81 +27,101 @@ class vec2:
         self.x = x
         self.y = y
 
+    def copy(self) -> 'vec2':
+        return vec2(self.x, self.y)
 
-def print_tail_positions(tail_list: set[tuple[int, int]]) -> None:
-    # minimum to rescale to 0,0 in bottom left corner
-    start_x = min([point[0] for point in tail_list])
-    start_y = min([point[1] for point in tail_list])
+    def __eq__(self, point: 'vec2') -> bool:
+        return self.x == point.x and self.y == point.y
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y)) # hash tuple of coordinates
+
+    # operator for square bracket indexing
+    def __getitem__(self, index: int) -> int:
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+        else:
+            raise IndexError("Index out of range")
+
+
+def print_tail_positions(pos_list: list[vec2]) -> None:
+    # minimum to rescale bottom left corner to (0, 0)
+    min_x = min([point.x for point in pos_list])
+    min_y = min([point.y for point in pos_list])
     # size of grid accounting for rescaled minimum
-    size_x = max([point[0] for point in tail_list]) - start_x + 1
-    size_y = max([point[1] for point in tail_list]) - start_y + 1
+    size_x = max([point.x for point in pos_list]) - min_x + 1
+    size_y = max([point.y for point in pos_list]) - min_y + 1
     # create and fill grid
-    grid = [[False for _ in range(size_x)] for _ in range(size_y)]
-    for point in tail_list:
-        point_x = point[0] - start_x
-        point_y = point[1] - start_y
-        grid[point_y][point_x] = True
+    grid = [[None for _ in range(size_x)] for _ in range(size_y)]
+    chars = "0123456789abcdefghijklmnopqrstuvwxyz"  # chars to assign
+    for i, point in zip(range(len(pos_list)), pos_list):
+        row = point.y - min_y
+        col = point.x - min_x
+        grid[row][col] = chars[i % len(chars)]
     # print grid
-    for i in range(len(grid)-1, -1, -1):  # 0,0 is bottom left
+    for i in reversed(range(len(grid))):
         for j in range(len(grid[i])):
-            if i == -start_y and j == -start_x:
-                print("o", end="")
-            elif grid[i][j]:
-                print("x", end="")
+            if i == (0 - min_y) and j == (0 - min_x):
+                print("@", end="")  # starting position
+            elif grid[i][j] is not None:
+                print(f"{grid[i][j]}", end="")
             else:
                 print(".", end="")
         print()
 
 
-def get_tail_positions(moves: list[tuple[str, int]], knot_count: int) -> int:
+def tail_positions(moves: list[tuple[str, int]], knot_count: int, verbosity: int) -> list[vec2]:
     if (knot_count < 2):
-        raise ValueError(f"knot_count must be at least 2, not {knot_count}")
+        knot_count = 2
+        print("WARNING: knot count must be at least 2, defaulting to 2")
 
-    tail_list = set()
-    knots = [vec2(0, 0) for _ in range(knot_count)]
+    tail_pos_list = list()
+    knots = [vec2(0, 0) for _ in range(knot_count)] # all stacked on top of each other
 
     for move in moves:
         direction, distance = move[0], move[1]
         for _ in range(distance):
             # move leading head position
             head = knots[0]
-            if direction == "U":
-                head.y += 1
-            elif direction == "D":
-                head.y -= 1
-            elif direction == "L":
-                head.x -= 1
-            elif direction == "R":
-                head.x += 1
+            if    direction == "U":  head.y += 1
+            elif  direction == "D":  head.y -= 1
+            elif  direction == "L":  head.x -= 1
+            elif  direction == "R":  head.x += 1
             # move subsequent head/tail pairs
             for i in range(1, len(knots)):
                 head = knots[i-1]
                 tail = knots[i]
                 if max(abs(head.x - tail.x), abs(head.y - tail.y)) > 1:
-                    if head.x > tail.x:
-                        tail.x += 1
-                    elif head.x < tail.x:
-                        tail.x -= 1
-                    if head.y > tail.y:
-                        tail.y += 1
-                    elif head.y < tail.y:
-                        tail.y -= 1
+                    if    head.x > tail.x:  tail.x += 1
+                    elif  head.x < tail.x:  tail.x -= 1
+                    if    head.y > tail.y:  tail.y += 1
+                    elif  head.y < tail.y:  tail.y -= 1
             # add last knot in rope to set of unique positions
-            tail_list.add((knots[-1].x, knots[-1].y))  # (x, y) tuple
+            tail_pos_list.append(knots[-1].copy())
 
-    print_tail_positions(tail_list)
-    return len(tail_list)
+    if verbosity > 0:
+        print_tail_positions(tail_pos_list)
 
-
-def part1(moves: list[tuple[str, int]]) -> int:
-    return get_tail_positions(moves, 2)
-
-
-def part2(moves: list[tuple[str, int]]) -> int:
-    return get_tail_positions(moves, 10)
+    return tail_pos_list
 
 
 if __name__ == '__main__':
-    data = parse_data("test2.txt")
-    print(f"part 1: {part1(data)}")
-    print(f"part 2: {part2(data)}")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", "-i", default="test.txt")
+    parser.add_argument("--verbose", "-v", action="count", default=0)
+    parser.add_argument("--knot-count", "-n", type=int, default=2)
+    args = parser.parse_args()
+
+    data = parse_data(args.input)
+
+    if args.verbose > 0:
+        tail_positions(data, args.knot_count, args.verbose)
+        print()
+
+    part1 = len(set(tail_positions(data, 2, 0)))
+    print(f"part 1: {part1}")
+
+    part2 = len(set(tail_positions(data, 10, 0)))
+    print(f"part 2: {part2}")
